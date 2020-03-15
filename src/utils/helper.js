820b74a25ecac7de68b7b67835cb3d2a1a176e60
yb <abo@live.hk>
2020-03-15 14:50:35 +0800

    移除 timespan 模块；修复 0 秒的 bug；

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,10 +1,18 @@
-import TimeSpan from 'timespan';
-
 export const formatDuration = (seconds) => {
-  let time = TimeSpan.fromSeconds(seconds);
-  let text = `${time.seconds} 秒`;
-  if (time.minutes) text = `${time.minutes} 分 ${text}`;
-  if (time.hours) text = `${Math.floor(time.totalHours())} 小时 ${text}`;
+  let s = parseInt(seconds);
+  let m = 0;
+  let h = 0;
+  if (s >= 60) {
+    m = parseInt(s / 60);
+    s = parseInt(s % 60);
+    if (m >= 60) {
+      h = parseInt(m / 60);
+      m = parseInt(m % 60);
+    }
+  }
+  let text = `${s} 秒`;
+  if (m > 0) text = `${m} 分 ${text}`;
+  if (h > 0) text = `${h} 小时 ${text}`;
   return text;
 }
 
