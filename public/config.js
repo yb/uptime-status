255478fdf5efb7419201ebc6666863663da2ff3f
Burial0268 <71890055+Burial0268@users.noreply.github.com>
2021-04-11 18:03:57 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -11,15 +11,12 @@ window.Config = {
   // 只需填写域名部分，默认为官网域名
   // 因官网 API 时不时的会 CROS 报错，可填自定义反代域名
   // 详见 https://github.com/yb/uptime-status/ 说明
-  ApiDomain: 'api.uptimerobot.com',
+  ApiDomain: 'gh.burial0268.tk',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur1266774-caa83060601101b3dd8171a1',
   ],
 
   // 是否显示监测站点的链接
@@ -33,11 +30,11 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: '博客首页',
+      url: 'https://burial0268.tk/'
     },
     {
-      text: 'GitHub',
+      text: 'GitHub by uptime',
       url: 'https://github.com/yb/uptime-status'
     }
   ]
