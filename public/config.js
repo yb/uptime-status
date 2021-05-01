9a8af36c69833a535fc629a0336b7a5c647f7838
Scholar-m <49067853+Scholar-m@users.noreply.github.com>
2021-05-01 11:03:18 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -16,10 +16,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'u1196020-0603afcf216b56ff6d3a09b5',
   ],
 
   // 是否显示监测站点的链接
