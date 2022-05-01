201fd28db0438f57a7b36007c4a959dc7e079b37
pai233 <gugugupai233@gmail.com>
2022-05-01 12:41:00 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -2,7 +2,7 @@
 window.Config = {
 
   // 站点名
-  SiteName: 'Uptime Status',
+  SiteName: 'Pai233\'s Service Status',
 
   // 站点链接
   SiteUrl: '/',
@@ -11,15 +11,12 @@ window.Config = {
   // 只需填写域名部分，默认为官网域名
   // 因官网 API 时不时的会 CROS 报错，可填自定义反代域名
   // 详见 https://github.com/yb/uptime-status/ 说明
-  ApiDomain: 'api.uptimerobot.com',
+  ApiDomain: 'api.status.pai233.top',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur1717569-d70b39bf1c1023f180479e9a'
   ],
 
   // 是否显示监测站点的链接
@@ -33,12 +30,12 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: 'Blog',
+      url: 'https://blog.pai233.top/'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      url: 'https://github.com/pai233/'
     }
   ]
 };
