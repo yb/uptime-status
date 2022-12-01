0331b20b8d3ce2ec407216eb58b96e5ee4a71851
great99mm <49770257+great99mm@users.noreply.github.com>
2022-12-01 23:07:27 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,10 +6,8 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm793167828-9bfea239456b3f4cbf142d24',
+    'm793167823-2fcd6c0eb04289ee901e59aa',
   ],
 
   // 日志天数
