6a73d4f9f4d836f3693dec10cbe7d4060d933b8e
Ilya <117220282+YakkaDev@users.noreply.github.com>
2023-03-10 11:20:35 +0200

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,39 @@
+window.Config = {
+  // Menu-bar Name
+  SiteName: 'Public Status',
+
+  // UptimeRobot Api Keys
+  // Monitor-Specific  Read-Only
+  ApiKeys: [
+    'm793883641-2edcb5af15f7045abf2c2d24',
+    'm793883641-2edcb5af15f7045abf2c2d24',
+    'm793883653-1b266f7b6cfff58a7ae0d8a6',
+    'm793883663-3322481ed2fbd4ba8909121f',
+    'm793883657-a545f0ff086e21c109642d42',
+    'm793883659-6102eba0535e538cc4c09fbe',
+    'm793883688-53ef88a59a4e5c3974f5893d',
+    'm793883696-97c0f6c75728ffed84a6d947',
+  ],
+
+  // Uptime duration
+  CountDays: 90,
+
+  // Uptime links
+  ShowLink: true,
+
+  // Menu-bar buttons
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://status.org.cn/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/yb/uptime-status'
+    },
+    {
+      text: 'Blog',
+      url: 'https://abo.xyz/'
+    },
+  ],
+};
