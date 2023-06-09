fd0c592395740118f8dd64d67505becba1f99308
doveyoung <doveyoung@outlook.com>
2023-06-09 10:37:16 +0800

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app';
 import './app.scss';
-
+const customapi = process.env.API_KEY;
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(<App />);
 // root.render(
